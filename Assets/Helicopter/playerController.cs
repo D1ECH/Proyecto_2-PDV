000060c8a752ab2887b209840b1656b4e86de149
Diego González Rodríguez <diechgr48@gmail.com>
2024-05-13 19:24:13 +0200

    primera subida
    prueba a subir el codigo a github

---
+++ Assets/Helicopter/playerController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public CharacterController cc;  // Referencia al CharacterController que controla al jugador
+    public float Velocidad = 12;    // Velocidad de movimiento del jugador (ajustabe desde el inspector)
+
+    public float Gravedad;      // Valor de la gravedad
+    public Vector3 velocity;    // Vector de velocidad del jugador
+
+    public Transform groundCheck;       // Punto de comprobación de suelo para verificar si el jugador está en el suelo
+    public float groundDistance = 0.4f; // Distancia de comprobación de suelo
+    public LayerMask floorMask;         // Máscara de capa para la detección de suelo
+    bool isGrounded;                    // Variable booleana que indica si el jugador está en el suelo
+
+    void Update()
+    {
+        // Verifica si el jugador está en el suelo
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, floorMask);
+
+        
+        // Si el jugador está en el suelo y la velocidad en y es menor que 0
+        if (isGrounded && velocity.y < 0)
+        {
+            // Resetea la velocidad en y a un valor pequeño para evitar que se hunda en el suelo
+            velocity.y = -2f;
+        }
+
+        // Si se presiona el botón de salto y el jugador está en el suelo
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Calcula la velocidad en y necesaria para realizar un salto
+            velocity.y = Mathf.Sqrt(6 * -2 * Gravedad);
+        }
+
+        
+        // Obtiene los valores de entrada horizontal y vertical para el movimiento
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Calcula el vector de movimiento en función de la entrada del jugador y la dirección del objeto
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Mueve al jugador según el vector de movimiento y la velocidad del jugador
+        cc.Move(move * Velocidad * Time.deltaTime);
+
+        // Aplica la gravedad al jugador
+        velocity.y += Gravedad * Time.deltaTime;
+
+        // Mueve al jugador según la velocidad en y
+        cc.Move(velocity * Time.deltaTime);
+    }
+}
