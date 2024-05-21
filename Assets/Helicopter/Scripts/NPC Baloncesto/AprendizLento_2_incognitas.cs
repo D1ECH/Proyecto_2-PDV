b2959650d0fcb4ac50e13b970aa7474c0f8f9a1f
Antonio Manuel Pavón Villar <antoniomanuel21601@uma.es>
2024-05-21 20:09:37 +0200

    Mejora baloncesto con trigger

--- Assets/Helicopter/Scripts/NPC Baloncesto/AprendizLento_2_incognitas.cs
+++ Assets/Helicopter/Scripts/NPC Baloncesto/AprendizLento_2_incognitas.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using weka.classifiers.trees;
 using weka.classifiers.evaluation;
 using weka.core;
@@ -21,7 +20,7 @@ public class AprendizLento_2_incognitas : MonoBehaviour
     public GameObject pelota;
     public GameObject PuntoObjetivo;
     GameObject InstanciaPelota;
-    public float valorMaximoFx=20, valorMaximoFy=20, paso=1, Velocidad_Simulacion = 1;
+    public float valorMaximoFx = 20, valorMaximoFy = 20, paso = 1, Velocidad_Simulacion = 1;
     public float valorMinimoFx = 16, valorMinimoFy = 10;
     float mejorFuerzaX, mejorFuerzaY, distanciaObjetivo;
     Rigidbody r;
@@ -42,47 +41,55 @@ public class AprendizLento_2_incognitas : MonoBehaviour
 
     IEnumerator Entrenamiento()
     {
-
-        //Uso de una tabla vacía:
-        //casosEntrenamiento = new weka.core.Instances(new java.io.FileReader("Assets/Iniciales_Experiencias.arff"));  //Lee fichero con variables. Sin instancias
-
-        //Uso de una tabla con los datos del último entrenamiento:
         casosEntrenamiento = new weka.core.Instances(new java.io.FileReader("Assets/Finales_Experiencias.arff"));    //... u otro con muchas experiencias
 
         if (casosEntrenamiento.numInstances() < 10)
         {
-            // texto.text = "ENTRENAMIENTO: crea una tabla con las fuerzas Fx y Fy utilizadas y las distancias alcanzadas.";
             print("Datos de entrada: valorMaximoFx=" + valorMaximoFx + " valorMaximoFy=" + valorMaximoFy + "  " + ((valorMaximoFx == 0 || valorMaximoFy == 0) ? " ERROR: alguna fuerza es siempre 0" : ""));
-            for (float Fx = 16; valorMinimoFx <= valorMaximoFx; Fx = Fx + paso)                      //Bucle de planificación de la fuerza FX durante el entrenamiento
+            for (float Fx = valorMinimoFx;  Fx <= valorMaximoFx; Fx = Fx + paso)                      //Bucle de planificación de la fuerza FX durante el entrenamiento
             {
                 for (float Fy = valorMinimoFy; Fy <= valorMaximoFy; Fy = Fy + paso)                    //Bucle de planificación de la fuerza FY durante el entrenamiento
                 {
-                    // InstanciaPelota = Instantiate(pelota, npcTransform) as GameObject;
                     InstanciaPelota = Instantiate(pelota, this.transform.position, this.transform.rotation);
-                    print("INSTANCIA PELOTA");
                     Rigidbody rb = InstanciaPelota.GetComponent<Rigidbody>();              //Crea una pelota física
-                    print("RIGIDBODY");
                     rb.AddForce(new Vector3(-Fx, Fy, 0), ForceMode.Impulse);                //y la lanza con las fuerza Fx y Fy
-                    print("FUERZA");
 
                     float startTime = Time.time; // Guarda el tiempo inicial
-                    yield return new WaitUntil(() => rb.transform.position.y <= 10f || Time.time - startTime >= 10f);       //... y espera a que la pelota llegue al suelo
-                    print("WAIT UNTIL");
+                    bool hitTarget = false;
+                    PuntoObjetivo.GetComponent<Collider>().isTrigger = true;
+
+                    yield return new WaitUntil(() =>
+                    {
+                        // Verifica si el tiempo ha excedido 10 segundos
+                        if (Time.time - startTime >= 10f)
+                        {
+                            return true;
+                        }
+
+                        // Verifica si la pelota ha tocado el PuntoObjetivo
+                        if (PuntoObjetivo.GetComponent<Collider>().bounds.Intersects(rb.GetComponent<Collider>().bounds))
+                        {
+                            print("LA PELOTA HA TOCADO LA CANASTA");
+                            hitTarget = true;
+                            return true;
+                        }
+
+                        // Verifica si la pelota ha caído al suelo
+                        return rb.transform.position.y <= 10f;
+                    });
 
 
                     Instance casoAaprender = new Instance(casosEntrenamiento.numAttributes());
-                    print("ENTRENAMIENTO: con fuerza Fx " + Fx + " y Fy=" + Fy + " se alcanzó una distancia de " + rb.transform.position.x + " m");
                     casoAaprender.setDataset(casosEntrenamiento);                          //crea un registro de experiencia
                     casoAaprender.setValue(0, Fx);                                         //guarda los datos de las fuerzas Fx y Fy utilizadas
                     casoAaprender.setValue(1, Fy);
-                    casoAaprender.setValue(2, rb.transform.position.x);                    //anota la distancia alcanzada
+                    casoAaprender.setValue(2, hitTarget ? 1.0 : 0.0);                      //anota si alcanzó el objetivo
                     casosEntrenamiento.add(casoAaprender);                                 //guarda el registro en la lista casosEntrenamiento
                     rb.isKinematic = true; rb.GetComponent<Collider>().isTrigger = true;   //...opcional: paraliza la pelota
-                    Destroy(InstanciaPelota, 1);                                              //...opcional: destruye la pelota
+                    Destroy(InstanciaPelota, 1);                                           //...opcional: destruye la pelota
 
                 }                                                                          //FIN bucle de lanzamientos con diferentes de fuerzas
             }
-
 
             File salida = new File("Assets/Finales_Experiencias.arff");
             if (!salida.exists())
@@ -149,7 +156,7 @@ public class AprendizLento_2_incognitas : MonoBehaviour
                 Instance casoPrueba = new Instance(casosEntrenamiento.numAttributes());
                 casoPrueba.setDataset(casosEntrenamiento);
                 casoPrueba.setValue(1, Fy);                                                                   //crea un registro con una Fy
-                casoPrueba.setValue(2, distanciaObjetivo);                                                    //y la distancia
+                casoPrueba.setValue(2, 1.0);                                                                  //assuming perfect hit
                 float Fx = (float)saberPredecirFuerzaX.classifyInstance(casoPrueba);                          //Predice Fx a partir de la distancia y una Fy 
                 if ((Fx >= 1) && (Fx <= valorMaximoFx))
                 {
@@ -158,9 +165,9 @@ public class AprendizLento_2_incognitas : MonoBehaviour
                     casoPrueba2.setValue(0, Fx);                                                                 //Crea una registro con una Fx
                     casoPrueba2.setValue(1, Fy);                                                                 //Crea una registro con una Fy
                     float prediccionDistancia = (float)saberPredecirDistancia.classifyInstance(casoPrueba2);     //Predice la distancia dada Fx y Fy
-                    if (Mathf.Abs(prediccionDistancia - distanciaObjetivo) < menorDistancia)                     //Busca la Fy con una distancia más cercana al objetivo
+                    if (Mathf.Abs(prediccionDistancia - 1) < menorDistancia)                                   //Busca la Fy con una distancia más cercana al objetivo
                     {
-                        menorDistancia = Mathf.Abs(prediccionDistancia - distanciaObjetivo);                     //si encuentra una buena toma nota de esta distancia
+                        menorDistancia = Mathf.Abs(prediccionDistancia - 1);                                   //si encuentra una buena toma nota de esta distancia
                         mejorFuerzaX = Fx;                                                                       //de la fuerzas que uso, Fx
                         mejorFuerzaY = Fy;                                                                       //tambien Fy
                         print("RAZONAMIENTO: Una posible acción es ejercer una fuerza Fx=" + mejorFuerzaX + " y Fy= " + mejorFuerzaY + " se alcanzaría una distancia de " + prediccionDistancia);
@@ -183,15 +190,13 @@ public class AprendizLento_2_incognitas : MonoBehaviour
         }
         if (ESTADO == "Acción realizada")
         {
-            // texto.text = "Para una canasta a " + distanciaObjetivo.ToString("0.000") + " m, las fuerzas Fx y Fy a utilizar será: " + mejorFuerzaX.ToString("0.000") + "N y " + mejorFuerzaY.ToString("0.000") + "N, respectivamente";
-            if (r.transform.position.y < 0)                                            //cuando la pelota cae por debajo de 0 m
-            {                                                                          //escribe la distancia en x alcanzada
-                print("La canasta está a una distancia de " + distanciaObjetivo + " m");
+            if (Vector3.Distance(r.transform.position, PuntoObjetivo.transform.position) <= 1f)                    //cuando la pelota alcanza el objetivo
+            {                                                                                                      //escribe la distancia en x alcanzada
+                print("La pelota alcanzó el objetivo.");
                 print("La pelota lanzada llegó a " + r.transform.position.x + ". El error fue de " + (r.transform.position.x - distanciaObjetivo).ToString("0.000000") + " m");
                 r.isKinematic = true;
                 ESTADO = "FIN";
             }
         }
     }
-
 }
